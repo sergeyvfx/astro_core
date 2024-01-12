eb8179ce20f46d5d8dfad35b20bb2d37b15ff91f
Sergey Sharybin <sergey.vfx@gmail.com>
2024-01-12 02:50:13 +0100

    Move horizontal coordinate test code from comment to script
    Makes it more convenient to run the code when looking into the
    regression test.

--- source/astro_core/coordinate/internal/horizontal_test.cc
+++ source/astro_core/coordinate/internal/horizontal_test.cc
@@ -19,68 +19,8 @@ class HorizontalTest : public testing::Test {
 };
 
 TEST_F(HorizontalTest, FromITRF_Quad2) {
-  // Tested against the Astropy code:
-  //
-  // #!/usr/bin/env python3
-  //
-  // from astropy.coordinates import (
-  //     EarthLocation,
-  //     ITRS,
-  //     CartesianDifferential,
-  //     CartesianRepresentation,
-  //     AltAz,
-  // )
-  // from astropy import units as u
-  // from astropy.time import Time
-  // import numpy as np
-  //
-  // np.set_printoptions(precision=18, suppress=True)
-  //
-  // t = Time("2022-08-10T22:00:00.000", scale="utc", format="isot")
-  //
-  // # Position of GOES 16.
-  // itrs_p_data = (
-  //     10366753.696330964565277100,
-  //     -40872357.989425994455814362,
-  //     -7177.146307127579348162,
-  // )
-  // itrs_v_data = (
-  //     -0.000000123270733281970,
-  //     -0.000000265212595462799,
-  //     0.000000614554703409340,
-  // )
-  //
-  // # Construct ITRS frame of the GOES 16 satellite.
-  // itrs_p = CartesianRepresentation(np.array(itrs_p_data) * u.m)
-  // itrs_v = CartesianDifferential(np.array(itrs_v_data) * u.m / u.s)
-  // itrs = ITRS(itrs_p.with_differentials(itrs_v), obstime=t)
-  //
-  // # Earth locations of sites to calculate Azimuth/elevation from.
-  // all_sites = [
-  //     # Siding Spring observatory.
-  //     EarthLocation.of_site("aao"),
-  //     # Somewhere in Europe.
-  //     EarthLocation(lat=50 * u.deg, lon=8 * u.deg),
-  // ]
-  //
-  // for site in all_sites:
-  //     site_itrs = site.itrs
-  //     site_itrs_p = site_itrs.earth_location
-  //     print(
-  //         "Site : ("
-  //         f"{site_itrs_p.x.value:0.18f}, "
-  //         f"{site_itrs_p.y.value:0.18f}, "
-  //         f"{site_itrs_p.z.value:0.18f})"
-  //     )
-  //
-  //     aa = itrs.transform_to(AltAz(obstime=t, location=site_itrs_p))
-  //
-  //     print(f"Elevation: {aa.alt}")
-  //     print(f"Azimuth: {aa.az}")
-  //     print(f"Distance: {aa.distance}")
-  //     print()
-  //
-  // The code above produces the following output:
+  // Tested against the Astropy code in horizontal_test.py, which produces the
+  // following output:
   //
   //   Site : (-4680888.602721117436885834, 2805218.446534293703734875,
   //   -3292788.080450601410120726)
